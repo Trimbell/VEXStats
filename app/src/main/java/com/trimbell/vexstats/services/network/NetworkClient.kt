1d41af51cb375f9eaa0c3d5946ce4fa1b7cd4167
Mason Trimbell <mtmaster3@gmail.com>
2019-08-06 11:31:59 -0500

    Add NetworkClient

---
+++ app/src/main/java/com/trimbell/vexstats/services/network/NetworkClient.kt
@@ -0,0 +1,32 @@
+package com.trimbell.vexstats.services.network
+
+import java.util.concurrent.TimeUnit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkClient {
+
+    private val BASE_URL = "https://vexdb.io"
+    private val TIMEOUT = 10
+    var retrofit: Retrofit? = null
+    /*
+    This public static method will return Retrofit client
+    anywhere in the application
+    */
+    val retrofitClient: Retrofit
+        get() {
+            if (retrofit == null) {
+                val okHttpClientBuilder = OkHttpClient.Builder()
+                okHttpClientBuilder.connectTimeout(TIMEOUT.toLong(), TimeUnit.SECONDS)
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClientBuilder.build())
+                    .build()
+            }
+            return retrofit!!
+        }
+
+}
